73bc1ff94188611b9b5b660aa8f841525f7b976a
Bhavani-1234-m <bhavanimotukuri877@gmail.com>
2024-07-16 14:06:05 +0530

    Create 10.write a program to copy the elements of 1 array to other.C

---
+++ 10.write a program to copy the elements of 1 array to other.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a[6]={2,3,4,5,6};
+    int b[6],i;
+    for(i=0;i<6;i++)
+    {
+        b[i]=a[i];
+    }
+    printf("\n first array is:");
+    for(i=0;i<6;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    printf("\n econd array is:");
+    for(i=0;i<6;i++)
+    {
+        printf("%d\t",b[i]);
+    }
+}
